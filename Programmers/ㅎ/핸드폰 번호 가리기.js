e3a2b1a04772822ffb26f5b7207fc39113435bc1
misun <mcoding777@naver.com>
2022-04-22 13:43:52 +0900

    Add solution dc7c19dd

--- Programmers/ㅎ/핸드폰 번호 가리기.js
+++ Programmers/ㅎ/핸드폰 번호 가리기.js
@@ -20,3 +20,20 @@ https://programmers.co.kr/learn/courses/30/lessons/12948
 function solution(phone_number) {
   return phone_number.slice(-4).padStart(phone_number.length, "*");
 }
+
+// 두번째 풀이 -----------------------------------------------------------------------------------------------
+
+function solution(phone_number) {
+  return phone_number.replace(/\d(?=\d{4})/g, "*");
+}
+
+// 세번째 풀이 -----------------------------------------------------------------------------------------------
+
+function solution(phone_number) {
+  return "*".repeat(phone_number.length - 4) + phone_number.slice(-4);
+}
+
+/*
+★★★ 배운 점 ★★★
+- replace 메서드는 확실히 느리다. (첫번째 풀이와 세번째 풀이는 0.0X 속도였지만, 두번째 풀이는 0.X 속도)
+*/
