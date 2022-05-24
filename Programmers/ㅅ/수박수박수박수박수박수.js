d1a8b48b3dbfa149f68a828a40ccbd64cc553ed6
misun <mcoding777@naver.com>
2022-05-24 22:32:01 +0900

    Fix the solution 05c6350f

--- Programmers/ㅅ/수박수박수박수박수박수.js
+++ Programmers/ㅅ/수박수박수박수박수박수.js
@@ -21,3 +21,16 @@ function solution(n) {
   var answer = "";
   return answer.padStart(n, "수박");
 }
+
+// 두번째 풀이 -----------------------------------------------------------------------------------------------
+
+function solution(n) {
+  var answer = "수박";
+  return answer.repeat(n).substring(0, n);
+}
+
+/*
+★★★ 배운 점 ★★★
+- String.substring(a, b)는 String의 a부터 b-1까지 부분 문자열을 반환한다.
+- 첫번째 풀이가 빠르다.
+*/
